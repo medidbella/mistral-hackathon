4e20d231ba67284c457567407ec48158cebf952e
medidbella <mohamedidbellaali@gmail.com>
2026-02-28 19:57:51 +0000

    backend folder init

---
+++ backend/server.ts
@@ -0,0 +1,12 @@
+import fastify from "fastify";
+
+const server = fastify();
+
+server.listen({ port: 3000 }, (err, addr) => {
+    if (err) {
+        console.error(err);
+        process.exit(1);
+    }
+    console.log(`Server listening at ${addr}`);
+});
+
